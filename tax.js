169ba802e75fc9d8b7d8eea099291ef9708ec959
NSCSUPORT <JOBSJOBSGEATS@HOTMAIL.COM>
2025-03-07 03:06:55 -0300

    Update tax.js

--- tax.js
+++ tax.js
@@ -1,4 +1,4 @@
-// tax.js
+module.exports = { applyTransactionFee };// tax.js
 module.exports.applyTransactionFee = (value) => {
     const transactionFee = 0.05; // Taxa de 5% (pode ser configurável)
     const valueWithFee = value * (1 + transactionFee); // Aplica a taxa de 5%
