b066c4acaabe2e9fb95059824da7591f887cea91
NSCSUPORT <JOBSJOBSGEATS@HOTMAIL.COM>
2025-03-07 02:24:41 -0300

    Update holofi.sol

--- holofi.sol
+++ holofi.sol
@@ -1,19 +1,76 @@
-// Exemplo de contrato HoloFi
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract HoloFiPayment {
-    address public owner;
+interface IHoloFi {
+    function mint(address to, uint256 amount) external;
+    function burn(address from, uint256 amount) external;
+}
+
+contract HoloFiToken is IHoloFi {
+    string public name = "HoloFi Token";
+    string public symbol = "HFT";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
     
-    constructor() {
-        owner = msg.sender;
-    }
-    
-    function receivePayment() public payable {
-        require(msg.value > 0, "Pagamento deve ser maior que zero.");
+    address private owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can execute this");
+        _;
     }
 
-    // Retornar saldo do contrato
-    function getBalance() public view returns (uint) {
-        return address(this).balance;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    
+    constructor(uint256 initialSupply) {
+        owner = msg.sender;
+        mint(msg.sender, initialSupply);
+    }
+
+    function _transfer(address from, address to, uint256 value) internal {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(balanceOf[from] >= value, "ERC20: transfer amount exceeds balance");
+
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        emit Transfer(from, to, value);
+    }
+
+    function transfer(address to, uint256 value) external returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) external returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool) {
+        require(allowance[from][msg.sender] >= value, "ERC20: allowance exceeded");
+        allowance[from][msg.sender] -= value;
+        _transfer(from, to, value);
+        return true;
+    }
+
+    function mint(address to, uint256 amount) public override onlyOwner {
+        require(to != address(0), "ERC20: mint to the zero address");
+        totalSupply += amount;
+        balanceOf[to] += amount;
+        emit Transfer(address(0), to, amount);
+    }
+
+    function burn(address from, uint256 amount) public override onlyOwner {
+        require(from != address(0), "ERC20: burn from the zero address");
+        require(balanceOf[from] >= amount, "ERC20: burn amount exceeds balance");
+        
+        totalSupply -= amount;
+        balanceOf[from] -= amount;
+        emit Transfer(from, address(0), amount);
     }
 }
