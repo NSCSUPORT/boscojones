ea98e8f67e64f4b88d229b81c38cc6f049f11e61
NSCSUPORT <JOBSJOBSGEATS@HOTMAIL.COM>
2025-06-17 13:57:57 -0300

    Delete api directory

--- api/saldo.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./database.db');
-
-module.exports = (req, res) => {
-  db.get("SELECT saldo FROM wallet WHERE id = 1", [], (err, row) => {
-    if (err) return res.status(500).json({ error: "Erro ao consultar saldo." });
-    res.status(200).json({ saldo: row ? row.saldo : 0 });
-  });
-};
