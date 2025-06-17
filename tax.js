97e0e7c7665e75c16f1b2e279d67d83602473843
NSCSUPORT <JOBSJOBSGEATS@HOTMAIL.COM>
2025-06-17 12:02:30 -0300

    Delete tax.js

--- tax.js
+++
@@ -1,6 +0,0 @@
-module.exports = { applyTransactionFee };// tax.js
-module.exports.applyTransactionFee = (value) => {
-    const transactionFee = 0.05; // Taxa de 5% (pode ser configurável)
-    const valueWithFee = value * (1 + transactionFee); // Aplica a taxa de 5%
-    return valueWithFee;
-};
