2e683d42e1be93f56f415c47639f6e10d1057e98
NSCSUPORT <JOBSJOBSGEATS@HOTMAIL.COM>
2025-06-17 15:26:39 -0300

    Delete pix-payment-api.js

--- pix-payment-api.js
+++
@@ -1,8 +0,0 @@
-/backend
-  /controllers
-    pix-payment-api.js  // Arquivo que lida com a lógica do pagamento via Pix
-  /routes
-    pix-payment-routes.js  // Definição das rotas da API
-  /models
-    payment-model.js  // Modelo para registrar os pagamentos
-  server.js  // Arquivo principal que inicia o servidor e configura as rotas
